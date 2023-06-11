ea5cbfe6a9cbd6289f5e9038df9d003c8002cfd8
小丶k <kun771447@163.com>
2023-06-11 17:21:02 +0800

    pages

---
+++ src/pages/Customizer.jsx
@@ -0,0 +1,7 @@
+import { memo } from "react";
+
+const Customizer = memo(() => {
+  return <div className="head-text">Customizer</div>;
+});
+
+export default Customizer;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,7 @@
+import { memo } from "react";
+
+const Home = memo(() => {
+  return <div className="head-text">Home</div>;
+});
+
+export default Home;
